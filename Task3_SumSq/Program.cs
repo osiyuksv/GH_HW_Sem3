620681947e5c9d04c3b623795da527344a1af904
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-20 17:27:42 +0300

    Task3_SumSq

---
+++ Task3_SumSq/Program.cs
@@ -0,0 +1,12 @@
+﻿// вывести сумму квадратов для
+// числа N от 1 до N
+Console.WriteLine("Введите число N>0");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int SumSq = 0;
+
+while(count <= n)
+{
+  SumSq = SumSq + count * count;
+  count++;
+}Console.WriteLine(SumSq);

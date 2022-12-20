2d8d07e1f799f2e3f91f900e99ed0c4dd76d9217
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-20 15:38:38 +0300

    Task1_XY 2

--- Task1_XY/Program.cs
+++ Task1_XY/Program.cs
@@ -6,8 +6,14 @@ int x = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите координату Y");
 int y = Convert.ToInt32(Console.ReadLine());
 
-if((x == 0) && (y == 0)) Console.WriteLine("Введена точка с нулевыми координатами");
+if((x == 0) && (y == 0)) 
+{
+  Console.WriteLine("Введена точка с нулевыми координатами");
+}
+else
+{
 if((x >= 0) && (y >= 0)) Console.WriteLine(1);
 if((x <= 0) && (y >= 0)) Console.WriteLine(2);
 if((x <= 0) && (y <= 0)) Console.WriteLine(3);
 if((x >= 0) && (y <= 0)) Console.WriteLine(4);
+}

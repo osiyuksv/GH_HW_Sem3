e3012f3c58e9efde892680c2938abb9348b7b814
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-20 18:21:05 +0300

    Task3_SumSq 5 For

--- Task3_SumSq/Program.cs
+++ Task3_SumSq/Program.cs
@@ -2,14 +2,20 @@
 // числа N от 1 до N
 Console.WriteLine("Введите число N>0");
 int n = Convert.ToInt32(Console.ReadLine());
-int count = 1;
 int SumSq = 0;
 
-while(count <= n)
+Console.WriteLine("Сумма квадратов равна " + SumSq);
+
+for(int i = 1; i <= n; i++)
 {
-  SumSq = SumSq + count * count;
-  Console.Write(count * count);
-  Console.Write("; ");
-  count++;
+  SumSq = SumSq + i * i;
+  Console.Write(i * i + "; "); 
 }
 Console.WriteLine("Сумма квадратов равна " + SumSq);
+
+// while(count <= n)
+// {
+// SumSq = SumSq + count * count;
+// Console.Write(count * count + "; ");
+//count++;
+// }

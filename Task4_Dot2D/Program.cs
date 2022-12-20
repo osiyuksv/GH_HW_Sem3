557af5fb47be41be5e66a21fef0a4ce465834668
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-20 18:55:29 +0300

    Task4_Dot2D

---
+++ Task4_Dot2D/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вывести расстояние между двумя точками в 2D
+// пространстве по принятым координатам 2х точек
+Console.WriteLine("Введите x для точки 1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y для точки 1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите x для точки 2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите x для точки 2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double n = Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2));
+
+Console.WriteLine(n);

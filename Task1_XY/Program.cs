87b22489a34428d6f39e7348134c26c2d58fb649
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-20 15:35:31 +0300

    Task1_XY

---
+++ Task1_XY/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вывести номер четверти плоскости в которой находится 
+// точка с координатами X и Y при этом X и Y не = 0
+Console.WriteLine("Введите координату X");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if((x == 0) && (y == 0)) Console.WriteLine("Введена точка с нулевыми координатами");
+if((x >= 0) && (y >= 0)) Console.WriteLine(1);
+if((x <= 0) && (y >= 0)) Console.WriteLine(2);
+if((x <= 0) && (y <= 0)) Console.WriteLine(3);
+if((x >= 0) && (y <= 0)) Console.WriteLine(4);
